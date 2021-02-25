801847dd18499657a1ebd822afc65618bcc5da35
Clayton Jones <78233236+cgjones0711@users.noreply.github.com>
2021-02-25 13:24:07 -0600

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,20 @@
+
+document.getElementById("Button1").addEventListener("click", function Grow(){
+
+    document.getElementById("box").style.height = "250px";
+});
+
+    document.getElementById("Button2").addEventListener("click", function Blue(){
+
+        document.getElementById("box").style.backgroundColor = "blue";
+});
+
+document.getElementById("Button3").addEventListener("click", function Fade(){
+
+    document.getElementById("box").style.opacity = "0.5";
+});
+
+document.getElementById("Button4").addEventListener("click", function Reset(){
+
+    document.getElementById("box").style.height = "150px";
+});
